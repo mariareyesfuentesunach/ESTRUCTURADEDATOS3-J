6e184de8ae38c6ccd5ea733a9b133e0b0b4e74b8
Horacio09 <maria.reyes88@unach.mx>
2023-10-02 19:20:48 -0600

    P3_PYTHON Ejemplos de Arreglos

---
+++ P3_PYTHON Ejemplos de Arreglos/P3_PYTHON Ejemplos de Arreglos/P3_PYTHON_Ejemplos_de_Arreglos.py
@@ -0,0 +1,28 @@
+#Maria Jose Reyes Fuentes 3-j LSC
+numeros=[10,20,30,40,50,60]
+
+print(numeros)
+
+print(numeros[1])
+
+n=[3,4,5]
+
+print(n)
+
+print(n[2])
+
+c=["L", "U", "I", "S"]
+
+print(c)
+
+print(c[0])
+
+print(c[1])
+
+s="Mona Lisa"
+
+print(s)
+
+print(type(s))
+
+print(s[4])
