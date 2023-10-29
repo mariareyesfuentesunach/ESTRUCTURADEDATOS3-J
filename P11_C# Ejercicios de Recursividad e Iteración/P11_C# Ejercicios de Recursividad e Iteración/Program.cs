249c384dae45b7efcc0054db748dd5ef6e6e3617
Horacio09 <144492494+mariareyesfuentesunach@users.noreply.github.com>
2023-10-29 17:29:43 -0600

    P11_C# Ejercicios de Recursividad e Iteración

---
+++ P11_C# Ejercicios de Recursividad e Iteración/P11_C# Ejercicios de Recursividad e Iteración/Program.cs
@@ -0,0 +1,29 @@
+﻿// Maria Jose Reyes Fuentes 3-J LSC
+// Practica 11 - Ejercicios de Recursividad e Iteración 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace TABLA_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, I, RESULT;
+            string linea;
+            Console.Write("DIGITE NUMERO: "); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            I = 1;
+            while ((I <= 12))
+            {
+                RESULT = NUM * I;
+                Console.WriteLine("{0} * {1} = {2}", NUM, I, RESULT);
+                I = I + 1;
+            }
+            Console.Write("Pulse una tecla: "); Console.ReadLine();
+
+        }
+    }
+}
